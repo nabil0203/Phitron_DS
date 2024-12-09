36aa004a0143096cd690308a4723a981169bfd5d
nabil0203 <nabilahmed0203@gmail.com>
2024-12-09 09:53:41 +0600

    mod1

---
+++ mod-1/1.7_QuadraticCom.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+
+    // nested loop
+
+    /*
+     
+    if 2 loops are nested O(n^2)
+    if 3 loops are nested O(n^3)
+    if 4 loops are nested O(n^4)
+    
+    */
+
+
+    return 0;
+}
---
+++ mod-1/1.8_Linearithmic.cpp
@@ -0,0 +1,15 @@
+// nested
+// but if any loop's has increment/decrement in * or /
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+
+
+    return 0;
+}
