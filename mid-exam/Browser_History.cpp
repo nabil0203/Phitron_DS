adf3d30c467b7b3dac9eec6a54239df37c66b7c1
nabil0203 <nabilahmed0203@gmail.com>
2025-01-04 13:52:14 +0600

    update

--- mid-exam/Browser_History.cpp
+++ mid-exam/Browser_History.cpp
@@ -1,3 +1,47 @@
 // https://www.hackerrank.com/contests/mid-term-exam-a-basic-data-structures-a-batch-06/challenges/browser-history-1
 
+#include <bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    list<string> l;
+    string val;
+    while (1)
+    {
+        cin >> val;
+        if (val == "end")
+        {
+            break;
+        }
+        l.push_back(val);
+    }
+
+    int q;
+    cin >> q;
+    cin.ignore();
+
+    string a;
+
+    while (q--)
+    {
+        getline(cin, a);
+
+        stringstream ss(a);
+        string word;
+        ss >> word;
+
+        if (word == "visit")
+        {
+            
+        }
+        else if (word == "prev")
+        {
+        }
+        else if (word == "next")
+        {
+        }
+    }
+
+    return 0;
+}
